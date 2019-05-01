273041bb481696e7250511ec92c58bece2f07e2e
Tron-Tsai <48005162+Tron-Tsai@users.noreply.github.com>
2019-05-01 23:30:50 +0800

    Add files via upload

---
+++ homework2-Tron-Tsai-master/evil-extension/page.js
@@ -0,0 +1,36 @@
+const MATCH_LIST = {
+  'there': 'their',
+  'their': 'there',
+  'they\'re': 'there',
+  'There': 'Their',
+  'Their': 'There',
+  'They\'re': 'There',
+  'THERE': 'THEIR',
+  'THEIR': 'THERE',
+  'THEY\'RE': 'THERE'
+};
+
+function transformTextNodes(root, level) {
+  // TODO(you): Implement this function! See HW spec for details.
+  if (root.nodeType === Node.TEXT_NODE) {
+    console.log(level + root.textContent);
+    if(root.textContent.match(MATCH_LIST) != null ){
+      var string = root.textContent;
+      string = string.replace(/there|their|they\'re|Their|They\'re|THERE|THEIR|THEY\'RE/g, function(matched){
+        return MATCH_LIST[matched];
+      });
+      root.textContent = string;
+    }
+    console.log('wow');
+  } 
+  else {
+    console.log(level + root.nodeName);
+  }
+  for (const child of root.childNodes) {
+    transformTextNodes(child, level + "    ");
+  }
+}
+transformTextNodes(document.body,"");
+// Log statement to test that the extension loaded properly.
+console.log('Evil extension loaded!');
+console.log('Evil extension updated!');
